6dd61c11fcacd1a5f2d9757a3130ff8424928e1b
Aniruddha Sg <aniruddhasg@Aniruddhas-MacBook-Pro.local>
2026-01-04 23:11:07 +0530

    changed api

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3040/api';
+const API_URL = 'https://dct-app-quiz.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_URL,
